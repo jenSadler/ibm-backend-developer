7fdd56455bbddb9d2cef1ea0ea2097f1f40bbb9a
Jennifer Sadler <jennifer.sadler@gmail.com>
2024-10-24 23:57:37 -0400

    added some of my own tests

--- C5 - AI Applications with Python/unit-tests/test_mymodule.py
+++ C5 - AI Applications with Python/unit-tests/test_mymodule.py
@@ -1,7 +1,7 @@
 """Test the mymodule class"""
 
 import unittest
-from mymodule import square, double
+from mymodule import square, double, add
 
 class TestSquare(unittest.TestCase):
     """Test the square functions"""
@@ -19,5 +19,20 @@ class TestDouble(unittest.TestCase):
         self.assertEqual(double(2), 4) # test when 2 is given as input the output is 4.
         self.assertEqual(double(-3.1), -6.2) # test when -3.1 is given as input the output is -6.2)
         self.assertEqual(double(0), 0) # test when 0 is given as input the output is 0.
+
+class TestAdd(unittest.TestCase):
+    """Test doubles functions"""
+    def test1(self):
+        """Test doubling ints, floats and 0 """
+        # test when 2 and 2 are given as input the output is 4.
+        self.assertEqual(add(2, 2), 4)
+        # test when 3.1  and 1.4 are given as input the output is 4.5)
+        self.assertEqual(add(3.1, 1.4), 4.5)
+        # test when 0 and 0 are given as input the output is 0.
+        self.assertEqual(add(0, 0), 0)
+        # test when -1 and -1 are given as input the output is 2.
+        self.assertEqual(add(-1, -1), -2)
+        # test when -1 and -1 are given as input the output is 2.
+        self.assertNotEqual(add(5, 2), 8)
 
 unittest.main()
