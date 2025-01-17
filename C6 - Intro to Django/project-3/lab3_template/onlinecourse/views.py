0f661472492b025e6298f6245e8e5a90c4b28b7f
Jennifer Sadler <jennifer.sadler@gmail.com>
2025-01-17 12:17:25 -0500

    made sample django course app

---
+++ C6 - Intro to Django/project-3/lab3_template/onlinecourse/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render, redirect
+from .models import Course, Lesson, Enrollment
+from django.urls import reverse
+from django.views import generic
+from django.http import Http404
+
+# Create your views here.
+def popular_course_list(request):
+    context = {}
+    if request.method == 'GET':
+        course_list = Course.objects.order_by('-total_enrollment')[:10]
+        context['course_list'] = course_list
+        return render(request, 'onlinecourse/course_list.html',context)
+
+def enroll(request, course_id):
+    if request.method == 'POST':
+        course = get_object_or_404(Course, pk=course_id)
+        course.total_enrollment += 1
+        course.save()
+        return HttpResponseRedirect(reverse(viewname='onlinecourse:course_details', args=(course.id,)))
+def course_details(request, course_id):
+    context = {}
+    if request.method == 'GET':
+        try:
+            course = Course.objects.get(pk=course_id)
+            context['course'] = course
+            return render(request, 'onlinecourse/course_detail.html',context)
+        except Course.doesNotExist:
+            raise Http404("No Course matches that given id")
